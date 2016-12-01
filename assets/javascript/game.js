0927931187ab502767874b75f9fd01d952f29c3e
seamanhd <seamanhd@gmail.com>
2016-11-30 19:43:29 -0500

    cleaned up functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,22 +17,111 @@ var animalList = [
 // var guessesLeft = number of remaining guesses 
 
 
- var winCount = 0;
+ var winCount = "";
 
- var guessesLeft = 10;
+ var guessesLeft = "";
 
  var dashes =[];
 
  var wrongGuesses = [];
 	
-var currentAnimal = ""
+var currentAnimal = "";
 
+// ----------------------------------------------------------------------------
 // FUNCTIONS
 
 //reset function
 
+var reset = function () {
+	guessesLeft = 10;
+	dashes =[];
+	wrongGuesses=[];
+	currentAnimal="";
+}
 
 
+
+
+var setUp = function () {
+	// pick a random animal ----- not a global variable??
+		currentAnimal = animalList[Math.floor(Math.random() * animalList.length)];
+	console.log(currentAnimal);
+	//setting guess count
+		guessesLeft = 10
+	// split up letters of current animal
+	currentAnimal.split("");
+	for (var i=0;i<currentAnimal.length;i++) {console.log(currentAnimal[i])}
+	//create array of dashes
+	for (var i=0;i<currentAnimal.length;i++) {
+		dashes.push(" _ ");
+		
+	}
+	//write dashes to the page
+		document.querySelector("#current").innerHTML = "Current Word: " + dashes;
+
+}
+
+
+
+
+
+var checkGuessCount = function () {
+	if (guessesLeft>0) {
+			console.log("rungame");
+			guessesLeft--;
+		}
+
+		else  {
+			alert("Game Over. Try Again!");
+			reset();
+		}
+}
+
+
+
+
+var checkUserGuess = function () {
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		console.log(userGuess);
+
+	if (currentAnimal.indexOf(userGuess) > -1) {
+			console.log(true)
+			var guessIndex = currentAnimal.indexOf(userGuess);
+			console.log(guessIndex);
+			dashes.splice(guessIndex, 1, userGuess);
+			console.log(dashes)
+			document.querySelector("#current").innerHTML = "Current Word: " + dashes;
+		}
+		else {
+			console.log(false)
+			document.querySelector("#remaining").innerHTML = "Guesses remaining: " + guessesLeft;
+			wrongGuesses.push(" " + userGuess);
+			console.log(wrongGuesses);
+			document.querySelector("#guessed").innerHTML = "Already guessed: " + wrongGuesses;
+
+		}
+}
+
+
+
+var runGame = function () {
+	setUp();
+	document.onkeyup = function () {
+	checkGuessCount();
+	//var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		//onsole.log(userGuess);
+	checkUserGuess();
+	}
+}
+
+// --------------------------------------------------------------------------------------
+// THE ACTUAL GAME! 
+
+	runGame ();
+
+
+
+	/*
 	//prep the game 
 var runGame = function () {
 	// pick a random animal ----- not a global variable??
@@ -46,6 +135,7 @@ var runGame = function () {
 		dashes.push(" _ ");
 		
 	}
+	//write dashes to the page
 		document.querySelector("#current").innerHTML = "Current Word: " + dashes;
 
 
@@ -53,24 +143,30 @@ var runGame = function () {
 
 
 
-
+		//when user presses key ...
 	document.onkeyup = function () {
 
+		//if they have guesses left, take away one of their guesses
 		if (guessesLeft>0) {
 			console.log("rungame");
 			guessesLeft--;
 		}
 
-		else {
-			alert("Try Again");
-			window.location.reload();
+		else  {
+			endGame();
 		}
+
+
+
+		
+
+		
 	
-		//if statement check guessesLeft for >0, then subtract one 
-		//else statement end the game, run setUp
+		
 		// identify key that user guessed	
 		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 		console.log(userGuess);
+
 		//checking userGuess against the array
 		if (currentAnimal.indexOf(userGuess) > -1) {
 			console.log(true)
@@ -88,11 +184,17 @@ var runGame = function () {
 			document.querySelector("#guessed").innerHTML = "Already guessed: " + wrongGuesses;
 
 		}
+
+		//checking if they won!
+
+		if (dashes === currentAnimal) {
+			alert("You Win!!!");
+		}
 	}
 
 	}
 
-	runGame();
+	runGame();*/
 
 //how to replace the dashes? replace index value of dashes with index value of *new var* userIndex (index value of userGuess in currentAnimal) + write to html
 
